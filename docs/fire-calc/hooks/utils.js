a15cf52ce9e0a1190d0ba8171f291f7dca24736f
canwdev <9585126+canwdev@users.noreply.github.com>
2025-06-03 17:07:40 +0800

    u

--- docs/fire-calc/hooks/utils.js
+++ docs/fire-calc/hooks/utils.js
@@ -9,41 +9,51 @@ function convertMonthsToYearsAndMonths(months) {
   const duration = moment.duration(months, 'months');
   const years = Math.floor(duration.asYears());
   const remainingMonths = Math.floor(duration.asMonths()) % 12;
-  return `${years}年${remainingMonths}月`;
+
+  let result = ''
+  if (years !== 0) {
+    result += `${years}年`
+  }
+  if (remainingMonths !== 0) {
+    result += `${remainingMonths}月`
+  }
+  if (!result) {
+    result = '初始'
+  }
+  return result;
 }
 
 /** 数字金额大写转换(可以处理整数,小数,负数) */
-function numberToChineseMoney(n)
-{
+function numberToChineseMoney(n) {
   var fraction = ['角', '分'];
   var digit = ['零', '壹', '贰', '叁', '肆', '伍', '陆', '柒', '捌', '玖'];
-  var unit = [ ['元', '万', '亿'], ['', '拾', '佰', '仟']  ];
-  var head = n < 0? '欠': '';
+  var unit = [['元', '万', '亿'], ['', '拾', '佰', '仟']];
+  var head = n < 0 ? '欠' : '';
   n = Math.abs(n);
 
   var s = '';
 
-  for (var i = 0; i < fraction.length; i++)
-  {
+  for (var i = 0; i < fraction.length; i++) {
     s += (digit[Math.floor(n * 10 * Math.pow(10, i)) % 10] + fraction[i]).replace(/零./, '');
   }
   s = s || '整';
   n = Math.floor(n);
 
-  for (var i = 0; i < unit[0].length && n > 0; i++)
-  {
+  for (var i = 0; i < unit[0].length && n > 0; i++) {
     var p = '';
-    for (var j = 0; j < unit[1].length && n > 0; j++)
-    {
+    for (var j = 0; j < unit[1].length && n > 0; j++) {
       p = digit[n % 10] + unit[1][j] + p;
       n = Math.floor(n / 10);
     }
-    s = p.replace(/(零.)*零$/, '').replace(/^$/, '零')  + unit[0][i] + s;
+    s = p.replace(/(零.)*零$/, '').replace(/^$/, '零') + unit[0][i] + s;
   }
   return head + s.replace(/(零.)*零元/, '元').replace(/(零.)+/g, '零').replace(/^整$/, '零元整');
 }
 
 // https://stackoverflow.com/a/2901298
-function numberWithCommas(x) {
+function numberWithCommas(x, toFixedNum) {
+  if (toFixedNum && !Number.isNaN(x)) {
+    x = parseFloat(Number(x).toFixed(toFixedNum))
+  }
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
